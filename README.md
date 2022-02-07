842077942ff0d127d47c4cf8c9c9702bf5463573
hgko1207 <khkkhk1207@gmail.com>
2022-02-07 21:45:49 +0900

    readme update

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # React Js 마스터 클래스
 
+## 시작
+
+`npx` 명령어로 리액트 프로젝트를 생성하고, 필요 없는 파일들을 지워준다.
+src 폴더에서 **index.js** 와 **App.js** 파일을 제외한 나머지 파일들을 지운다.
+
+```bash
+npx create-react-app react-masterclass
+```
+
 ## Styled Components
 
 - https://styled-components.com/
--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex' }}>
+      <div style={{ backgroundColor: 'teal', width: 100, height: 100 }}></div>
+      <div style={{ backgroundColor: 'tomato', width: 100, height: 100 }}></div>
     </div>
   );
 }
