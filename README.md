4de6ad652f9a9937f8216f38394d410588d3c4ec
hgko1207 <khkkhk1207@gmail.com>
2022-02-08 22:29:03 +0900

    styled component

--- README.md
+++ README.md
@@ -16,3 +16,40 @@ npx create-react-app react-masterclass
 ```bash
 npm i styled-components
 ```
+
+사용 예
+
+```js
+import styled from 'styled-components';
+
+const Father = styled.div`
+  display: flex;
+`;
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+const Text = styled.span`
+  color: white;
+`;
+
+function App() {
+  return (
+    <Father>
+      <BoxOne>
+        <Text>Hello</Text>
+      </BoxOne>
+      <BoxTwo />
+    </Father>
+  );
+}
+export default App;
+```
+
+- Styled-Component 자동 완성을 위해 `vscode-styled-components` 플러그인을 설치한다.
--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import styled from 'styled-components';
 
+const Father = styled.div`
+  display: flex;
+`;
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+const Text = styled.span`
+  color: white;
+`;
+
 function App() {
   return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ backgroundColor: 'teal', width: 100, height: 100 }}></div>
-      <div style={{ backgroundColor: 'tomato', width: 100, height: 100 }}></div>
-    </div>
+    <Father>
+      <BoxOne>
+        <Text>Hello</Text>
+      </BoxOne>
+      <BoxTwo />
+    </Father>
   );
 }
 
