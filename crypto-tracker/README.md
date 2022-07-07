29e94890f3d62bcc74990489db7e62bc21f853a2
hgko1207 <khkkhk1207@gmail.com>
2022-07-07 23:01:34 +0900

    Setup

---
+++ crypto-tracker/README.md
@@ -0,0 +1,11 @@
+## 설치
+
+```bash
+npm i react-router-dom react-query
+```
+
+/ -> All coins
+/:id -> /btc -> Coin Detail
+
+/btc/information
+/btc/chart
---
+++ crypto-tracker/src/App.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+function App() {
+  return (
+    <Container>
+      <H1>protected</H1>
+    </Container>
+  );
+}
+
+export default App;
