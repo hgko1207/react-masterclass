a3802b348380821ad9bc68479f86ef543311bb6d
hgko1207 <khkkhk1207@gmail.com>
2022-07-15 22:49:14 +0900

    Home part

--- crypto-tracker/src/routes/Coins.tsx
+++ crypto-tracker/src/routes/Coins.tsx
@@ -1,11 +1,68 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+const Container = styled.div`
+  padding: 0px 10px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    padding: 20px;
+    transition: color 0.2s ease-in;
+    display: block;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
 const Title = styled.h1`
+  font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
 
+const coins = [
+  {
+    id: 'eth-ethereum',
+    name: 'Ethereum',
+    symbol: 'ETH',
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: 'coin',
+  },
+];
+
 function Coins() {
-  return <Title>코인</Title>;
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      <CoinsList>
+        {coins.map((coin) => (
+          <Coin key={coin.id}>
+            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+          </Coin>
+        ))}
+      </CoinsList>
+    </Container>
+  );
 }
 
 export default Coins;
