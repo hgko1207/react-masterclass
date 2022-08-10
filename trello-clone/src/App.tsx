0bbdafc605f30eb5f041440e19b7a2480455a002
hgko1207 <khkkhk1207@gmail.com>
2022-08-10 23:56:05 +0900

    Set Selectors

---
+++ trello-clone/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useRecoilState } from 'recoil';
+import { hourSelector, minuteState } from './atoms';
+
+function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+  };
+  return (
+    <div>
+      <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes" />
+      <input value={hours} onChange={onHoursChange} type="number" placeholder="Hours" />
+    </div>
+  );
+}
+
+export default App;
