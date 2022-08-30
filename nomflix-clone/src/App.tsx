275d1528da8495d1fbdc947cc783c124785f106f
hgko1207 <khkkhk1207@gmail.com>
2022-08-30 23:20:18 +0900

    Header

--- nomflix-clone/src/App.tsx
+++ nomflix-clone/src/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './Routes/Home';
+import Search from './Routes/Search';
+import Tv from './Routes/Tv';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
-        <Route path="/"></Route>
-        <Route path="/tv"></Route>
-        <Route path="/search"></Route>
+        <Route path="/tv" element={<Tv />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
     </Router>
   );
---
+++ nomflix-clone/src/Routes/Home.tsx
@@ -0,0 +1,7 @@
+function Home() {
+  return (
+    <div style={{ backgroundColor: 'whitesmoke', height: '200vh' }}>Home</div>
+  );
+}
+
+export default Home;
