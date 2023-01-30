6f27a8b1c56ab56d7bcbd53dc7250db9a08740a6
hgko1207 <khkkhk1207@gmail.com>
2023-01-30 23:42:44 +0900

    StaticImage

--- dev-stickers/src/pages/index.tsx
+++ dev-stickers/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { StaticImage } from 'gatsby-plugin-image';
 import * as React from 'react';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
@@ -5,9 +6,11 @@ import Seo from '../components/Seo';
 export default function IndexPage() {
   return (
     <Layout title="Welcome to DevStickers">
-      <div>
-        <h1></h1>
-      </div>
+      <StaticImage
+        height={500}
+        src="https://images.unsplash.com/photo-1625768376503-68d2495d78c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80"
+        alt="Stickers"
+      />
     </Layout>
   );
 }
