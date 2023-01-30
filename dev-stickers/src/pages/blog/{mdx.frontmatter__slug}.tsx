b163cf4326a4983e9fe70dd72d499039850e5df6
hgko1207 <khkkhk1207@gmail.com>
2023-01-30 22:57:00 +0900

    Blog Post

---
+++ dev-stickers/src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -0,0 +1,33 @@
+import { graphql } from 'gatsby';
+import React from 'react';
+import Layout from '../../components/Layout';
+import Seo from '../../components/Seo';
+
+interface IBlogPostProps {
+  data: Queries.PostDetailQuery;
+  children: any;
+}
+
+export default function BlogPost({ data, children }: IBlogPostProps) {
+  return (
+    <Layout title="">
+      <div>{children}</div>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      frontmatter {
+        author
+        category
+        date
+        slug
+        title
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }: IBlogPostProps) => <Seo title={data.mdx?.frontmatter?.title!} />;
