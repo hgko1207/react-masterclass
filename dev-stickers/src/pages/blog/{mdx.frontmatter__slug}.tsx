76aa508e6b09c05d648fdeb2842999aaac8feca7
hgko1207 <khkkhk1207@gmail.com>
2023-01-31 14:44:19 +0900

    GatsbyImage

--- dev-stickers/src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ dev-stickers/src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,4 +1,5 @@
 import { graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import React from 'react';
 import Layout from '../../components/Layout';
 import Seo from '../../components/Seo';
@@ -9,8 +10,10 @@ interface IBlogPostProps {
 }
 
 export default function BlogPost({ data, children }: IBlogPostProps) {
+  const image = getImage(data.mdx?.frontmatter?.headerImage?.childImageSharp?.gatsbyImageData!);
   return (
     <Layout title="">
+      <GatsbyImage image={image as any} alt={data.mdx?.frontmatter?.title!} />
       <div>{children}</div>
     </Layout>
   );
@@ -25,6 +28,11 @@ export const query = graphql`
         date
         slug
         title
+        headerImage {
+          childImageSharp {
+            gatsbyImageData(height: 450, placeholder: BLURRED)
+          }
+        }
       }
     }
   }
