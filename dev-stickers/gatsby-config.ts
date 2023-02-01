b899faaa60541d8cfa631b2ed68a36ed1abc494b
hgko1207 <khkkhk1207@gmail.com>
2023-02-01 09:34:48 +0900

    Contentful

--- dev-stickers/gatsby-config.ts
+++ dev-stickers/gatsby-config.ts
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
@@ -10,6 +11,14 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `qahf57g4n36d`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
--- dev-stickers/src/pages/index.tsx
+++ dev-stickers/src/pages/index.tsx
@@ -1,18 +1,35 @@
-import { StaticImage } from 'gatsby-plugin-image';
+import { graphql, PageProps } from 'gatsby';
+import { StaticImage, GatsbyImage, getImage } from 'gatsby-plugin-image';
 import * as React from 'react';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
 
-export default function IndexPage() {
+export default function IndexPage({ data }: PageProps<Queries.StickersQuery>) {
   return (
     <Layout title="Welcome to DevStickers">
-      <StaticImage
-        height={500}
-        src="https://images.unsplash.com/photo-1625768376503-68d2495d78c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80"
-        alt="Stickers"
-      />
+      {data.allContentfulStickerPack.nodes.map((sticker) => (
+        <article>
+          <GatsbyImage image={getImage(sticker.preview?.gatsbyImageData!)!} alt={sticker.name!} />
+          <h2>{sticker.name}</h2>
+          <h4>${sticker.price}</h4>
+        </article>
+      ))}
     </Layout>
   );
 }
+
+export const query = graphql`
+  query Stickers {
+    allContentfulStickerPack {
+      nodes {
+        name
+        price
+        preview {
+          gatsbyImageData(placeholder: BLURRED, height: 250)
+        }
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Home" />;
